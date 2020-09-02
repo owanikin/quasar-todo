80b91e82d97dfbdf75085dc2c7cf49ec778fbd7d
owanikin <ifeoluwa.oderinde@meltwater.org>
2020-09-02 19:37:24 +0100

    quasar commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae hic quidem minima, eos vitae iure explicabo voluptate aut dicta tempora ratione pariatur id est, voluptas itaque repudiandae quaerat, harum quod.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, illum. Tempora illo soluta consequuntur sit facere quam accusantium deserunt et voluptates neque repellat cum dolorem alias quasi voluptatum, laborum unde.</p>
+    
+  </q-page>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
